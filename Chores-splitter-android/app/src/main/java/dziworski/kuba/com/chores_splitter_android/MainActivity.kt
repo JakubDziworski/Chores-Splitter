f5e34c99d22b7f493f69f807644a9d73c94bf7ea
jakubdziworski <jakub.dziworski@gmail.com>
2017-04-29 13:10:13 +0200

    Android init

---
+++ Chores-splitter-android/app/src/main/java/dziworski/kuba/com/chores_splitter_android/MainActivity.kt
@@ -0,0 +1,13 @@
+package dziworski.kuba.com.chores_splitter_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
